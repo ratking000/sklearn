dc6c2ef6ea52be024c7b164e7cada9a2d1020e59
Joey <joey900302@gmail.com>
2023-07-09 22:49:28 +0800

    0709 test data

--- README.md
+++ README.md
@@ -12,4 +12,10 @@ LassoLars 8.7513
 Ridge  9.3730
 BayesianRidge 8.8234
 
-
+## 0709 繼續測試
+SVM(SVR) 13.2171
+Decision Tree 10.0090
+Random Forest 10.5705
+目前測起來好像都無法把rmse壓下來
+現在只有把y設為第一欄，但實際上會有六欄，這樣只有測一欄好像不準，全部測再平均?
+sklearn官網上的decision tree 中有提到 Multi-output 和 missing value support 之後研究
--- sklearn/imbd-template/2022_first/train.py
+++ sklearn/imbd-template/2022_first/train.py
@@ -5,13 +5,16 @@ import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model 
 from sklearn import svm
+from sklearn import tree
+from sklearn.ensemble import RandomForestClassifier
+
 from sklearn.pipeline import make_pipeline
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import cross_val_score,GridSearchCV
 from sklearn.metrics import mean_squared_error
 
 # Step 1: Read the CSV file
-df = pd.read_csv('/home/joey/program/sklearn/sklearn/imbd-template/2022_first/2022-train-v2.csv')
+df = pd.read_csv(r'C:\Users\User\sklearn\sklearn\imbd-template\2022_first\2022-train-v2.csv')
 
 # Step 2: Summary of the dataframe
 # The describe function only shows some fields, I want it to display all fields
@@ -31,23 +34,16 @@ df = df.dropna(axis=1)
 
 # Set y as the target variable, which is the first column of the dataframe
 y = df.iloc[:, 0]
-# print(y)
 # print(df.shape)
 
 # Set X as the feature variables, which are the remaining columns of the dataframe except the first 6 columns
-X = df.iloc[:, 6:]
-
-# column_names = df.columns.tolist()
-# x_column = column_names[0]
-# for column in column_names[6:8]:
-#     print(x_column)
-#     plt.plot(df[x_column], df[column], label=column)
+X = df.iloc[:, 6:]  
 
 # Split the data into trainning and testing sets
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=2023)
 
 # Use Linear Regression to train the model and test the model
-model = linear_model.ElasticNet()
+model = linear_model.LinearRegression()
 
 # model = make_pipeline(PolynomialFeatures(4), LinearRegression())
 
